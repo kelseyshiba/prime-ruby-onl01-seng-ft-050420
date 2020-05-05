0efc7dc6b25488b3035f90889c86853f0dbcac65
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-05 20:58:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,6 @@
 #what's a prime number?
 # n % 2 == 0 #by 1 or itself with R = 0
 # returns boolean
-def prime?(n)
-  if n == 2
-    return true
-  elsif n % 2 == 0 && n > 0 && n.odd?
-    return true 
-  else
-    false
-  end
-end
 
 def prime?(n)
   if n < 0 || n == 1 || n == 0 
