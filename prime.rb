290b5bd58b5e27ca083120f1b98b42370b6184e2
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-05 20:58:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+#what's a prime number?
+# n % 2 == 0 #by 1 or itself with R = 0
+# returns boolean
+
+def prime?(n)
+  if n < 0 || n == 1 || n == 0 
+    false
+  else
+    (2..n-1).to_a.all? do |num|
+      n % num != 0
+    end
+  end
+end
